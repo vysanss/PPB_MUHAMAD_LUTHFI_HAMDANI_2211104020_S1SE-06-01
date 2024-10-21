205654a4317146f04b6290d5e8d4f580b55d4990
Luthfi HM <123862774+vysanss@users.noreply.github.com>
2024-10-21 14:30:01 +0700

    Create custom_scroll_view.dart

---
+++ 05_Antarmuka_Pengguna_Lanjutan/GUIDED/custom_scroll_view.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class CustomScrollViewExample extends StatelessWidget {
+  const CustomScrollViewExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sliver"),
+        backgroundColor: Colors.amber,
+      ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          // Sliver App Bar
+          const SliverAppBar(
+            pinned: true,
+            expandedHeight: 50.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Sliver App Bar'),
+            ),
+          ),
+          // Sliver Grid
+          SliverGrid(
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200.0,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 4.0,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('Grid Item $index'),
+                );
+              },
+              childCount: 6,
+            ),
+          ),
+          // Sliver Fixed List
+          SliverFixedExtentList(
+            itemExtent: 50.0,
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.lightBlue[100 * (index % 9)],
+                  child: Text('List Item $index'),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
