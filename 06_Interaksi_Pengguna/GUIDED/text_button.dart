ceaccb9ca5bb55ef82ede5b5be7d0443781ea234
Luthfi HM <123862774+vysanss@users.noreply.github.com>
2024-10-23 21:31:06 +0700

    Create text_button.dart

---
+++ 06_Interaksi_Pengguna/GUIDED/text_button.dart
@@ -0,0 +1,8 @@
+TextButton(
+    onPressed: () {},
+    style: TextButton.styleFrom(
+        primary: Colors.blue, // Warna teks
+        backgroundColor: Colors.grey[200], // Warna latar belakang
+    ),
+    child: Text('Simpan'),
+)
