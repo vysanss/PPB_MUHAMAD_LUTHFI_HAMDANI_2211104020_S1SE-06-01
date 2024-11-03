ed6d0e1f46b618ac6a8f37f800103e6e20d5e56a
Luthfi HM <123862774+vysanss@users.noreply.github.com>
2024-11-03 17:15:16 +0700

    Update main.dart

--- 07_Navigasi_Notifikasi/UNGUIDED/main.dart
+++ 07_Navigasi_Notifikasi/UNGUIDED/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'E-Commerce App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
