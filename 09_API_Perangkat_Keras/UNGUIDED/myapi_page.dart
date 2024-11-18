88890256caf85c91ef8667bc28dd6a9a1b0c744f
Luthfi HM <123862774+vysanss@users.noreply.github.com>
2024-11-18 23:29:53 +0700

    Add files via upload

---
+++ 09_API_Perangkat_Keras/UNGUIDED/myapi_page.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class MyApiPage extends StatefulWidget {
+  @override
+  _MyApiPageState createState() => _MyApiPageState();
+}
+
+class _MyApiPageState extends State<MyApiPage> {
+  late CameraController _controller;
+  Future<void>? _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeControllerFuture = _initializeCamera();
+  }
+
+  Future<void> _initializeCamera() async {
+    final cameras = await availableCameras();
+    final firstCamera = cameras.first;
+
+    _controller = CameraController(
+      firstCamera,
+      ResolutionPreset.medium,
+    );
+
+    await _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Api Perangkat Keras"),
+        centerTitle: true,
+        backgroundColor: Color(0xFFFFD700),
+      ),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            // Pastikan kamera sudah diinisialisasi
+            await _initializeControllerFuture;
+            // Ambil gambar
+            final image = await _controller.takePicture();
+            // Tampilkan atau gunakan gambar
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    DisplayPictureScreen(imagePath: image.path),
+              ),
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: Icon(Icons.camera_alt),
+      ),
+    );
+  }
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({Key? key, required this.imagePath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Hasil Kamera'),
+      centerTitle: true,
+      backgroundColor: Color(0xFFFFD700)
+      ),
+      body: Image.file(File(imagePath)),
+    );
+  }
+}
