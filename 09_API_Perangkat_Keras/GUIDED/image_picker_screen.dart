06d336c50b28e6b563bf19bfc7e2afdbe44e955c
Luthfi HM <123862774+vysanss@users.noreply.github.com>
2024-11-18 18:43:58 +0700

    Create image_picker_screen.dart

---
+++ 09_API_Perangkat_Keras/GUIDED/image_picker_screen.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImageFromGalleryEx extends StatefulWidget {
+  final ImageSourceType type;
+  ImageFromGalleryEx(this.type);
+
+  @override
+  ImageFromGalleryExState createState() => ImageFromGalleryExState(this.type);
+}
+
+class ImageFromGalleryExState extends State<ImageFromGalleryEx> {
+  File? _image;
+  late ImagePicker imagePicker;
+  final ImageSourceType type;
+
+  ImageFromGalleryExState(this.type);
+
+  @override
+  void initState() {
+    super.initState();
+    imagePicker = ImagePicker();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(type == ImageSourceType.camera
+            ? "Image from Camera"
+            : "Image from Gallery"
+            ),
+      ),
+      body: Column(
+        children: <Widget>[
+          SizedBox(height: 52),
+          Center(
+            //mengambil gambar dari camera atau gallery
+            child: GestureDetector(
+              onTap: () async {
+                //operasi ternary untuk memilih sumber gambar
+                var source = type == ImageSourceType.camera
+                    ? ImageSource.camera
+                    : ImageSource.gallery;
+
+                //menyimpan gambar pada variabel image
+                XFile? image = await imagePicker.pickImage(
+                    source: source,
+                    imageQuality: 50,
+                    preferredCameraDevice: CameraDevice.front);
+
+                if (image != null) {
+                  setState(() {
+                    _image = File(image.path);
+                  });
+                }
+              },
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.red[200],
+                ),
+
+                // menampilkan gambar dari camera atau gallery
+                child: _image != null
+                    ? Image.file(
+                        _image!,
+                        width: 200.0,
+                        height: 200.0,
+                        fit: BoxFit.fitHeight,
+                      )
+
+                    // jika tidak ada gambar yang dipilih
+                    : Container(
+                        decoration: BoxDecoration(
+                          color: Colors.red[200],
+                        ),
+                        width: 200,
+                        height: 200,
+                        child: Icon(
+                          Icons.camera_alt,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+enum ImageSourceType { camera, gallery }
